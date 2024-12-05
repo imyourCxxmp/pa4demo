f6cbcee2ef84807528cc804f3e7c1eedce195249
imyourCxxmp <Chanawee19848@gmail.com>
2024-12-05 16:37:33 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -8,23 +8,25 @@ client = openai.OpenAI(api_key=user_api_key)
 
 prompt = """Act as an english exam writer who want undergraduate student to develop their reading comprehension skills.
 1.Your task is generating reading comprehension exam from keyword provided by user.
-2.You have to fact check and prove before using the information.
-3.The article should be 300-400 words , vocabulary should be B2-C1 following the CEFR level and grammar must correct and no error.
-4.The exam should be 10 questions with 4 multiple choice that complex and challenging that require deeper comprehension and interpretation skills.
+2.You have to fact check and prove with 2-3 sources before using the information.
+3.The article should be 300-500 words , vocabulary should be B2-C1 following the CEFR level and grammar must correct and no error.
+4.The article must contains linking words, 2 or more type of conjunction and provide additional contents which relate to the topic if avaliable.
+5.You must provide all link lead to references you took information form at the end of the article every time to respect the author.
+6.The exam should be 10 questions with 4 multiple choice that very difficult, complex and challenging that require deeper comprehension and interpretation skills.
 question should be in this structure:
 Question\n
 A)\n
 B)\n
 C)\n
 D)\n
-Thus, Question and choice shouldn't place in the same line.
-5.Then, you have to provide the answer key and description with proper reasons for every questions. 
+Thus, Question and Choice Shouldn't Place In The Same Line.
+7.Then, you have to provide the answer key and description with proper reasons for every questions. 
 title must be on 'Answer Key'. Also, the answer key should be in list of dictionaries. 
 It will be 2 column: Answer and Explanation as this structure:
 answer_key = [ {'Answer': '', 'Explanation': ""},
     ...........
     {'Answer': '', 'Explanation': ""} ]
-Because I will use this data to st.dataframe.
+Because I will use this data for st.dataframe.
 """
 
 st.title('Reading exam generator')
