cbd55a18478420b7bd4197c4c17e167e1abff3b5
imyourCxxmp <Chanawee19848@gmail.com>
2024-12-05 15:06:43 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -1,18 +1,23 @@
 import streamlit as st
 import pandas as pd
 import re
-import json
 import openai
 
 user_api_key = st.sidebar.text_input("OpenAI API key")
 client = openai.OpenAI(api_key=user_api_key)
+
 prompt = """Act as an english exam writer who want undergraduate student to develop their reading comprehension skills.
 1.Your task is generating reading comprehension exam from keyword provided by user.
 2.You have to fact check and prove before using the information.
 3.The article should be 300-400 words , vocabulary should be B2-C1 following the CEFR level and grammar must correct and no error.
 4.The exam should be 10 questions with 4 multiple choice that complex and challenging that require deeper comprehension and interpretation skills.
 question should be in this structure:
-Question\nA)\nB)\nC)\nD)\n
+Question
+A)
+B)
+C)
+D)
+Thus, Question and choice shouldn't place in the same line.
 5.Then, you have to provide the answer key and description with proper reasons for every questions. 
 title must be on 'Answer Key'. Also, the answer key should be in list of dictionaries. 
 It will be 2 column: Answer and Explanation as this structure:
@@ -23,7 +28,7 @@ Because I will use this data to st.dataframe.
 """
 
 st.title('Reading exam generator')
-st.markdown('AI ช่วยแต่งข้อสอบพาร์ต Reading เพียงแค่เขียน keyword ที่ต้องการ\nArticle จะมีความยาว 300-400 คำ ความยากระดับ B2-C1 และมีข้อสอบทั้งหมด 10 ข้อ')
+st.markdown('Hello, I will generate reading exam from your keyword texted in input. The article will be about 300-400 words, Difficulty is B2-C1 level follwing CEFR and questions will be 10.')
 user_input = st.text_input('text your keyword here', 'ex. Wicked')
 
 if st.button('Click'):
@@ -36,11 +41,17 @@ if st.button('Click'):
         messages=messages_so_far,
         temperature=1.2
     )
-    st.markdown('**AI response:**')
     answer = response.choices[0].message.content
     st.write(answer.split("answer_key = ")[0].strip())
-
     answer_string = answer.split("answer_key = ")[1].strip()
-    answer_json = json.loads(answer_string)
-    answer_df = pd.DataFrame(answer_json)
+    pattern = r"\{'Answer':\s*'([A-D])',\s*'Explanation':\s*\"(.*?)\"\}"
+    matches = re.findall(pattern, answer_string)
+    answer_key = [{'Answer': answer, 'Explanation': explanation} for answer, explanation in matches]
+    answer_df = pd.DataFrame(answer_key)
+    answer_df.index = ['1', '2', '3','4', '5', '6','7', '8', '9','10']
     st.dataframe(answer_df)
+    
+    sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+    selected = st.feedback("thumbs")
+    if selected is not None:
+        st.markdown(f"You selected: {sentiment_mapping[selected]}")
