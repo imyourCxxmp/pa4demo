9220141980214e3b67034ca6cb97aad47156b5fc
imyourCxxmp <Chanawee19848@gmail.com>
2024-12-05 14:51:26 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import re
+import json
 import openai
 
 user_api_key = st.sidebar.text_input("OpenAI API key")
@@ -11,25 +12,19 @@ prompt = """Act as an english exam writer who want undergraduate student to deve
 3.The article should be 300-400 words , vocabulary should be B2-C1 following the CEFR level and grammar must correct and no error.
 4.The exam should be 10 questions with 4 multiple choice that complex and challenging that require deeper comprehension and interpretation skills.
 question should be in this structure:
-Question\n
-A)\n 
-B)\n 
-C)\n 
-D)\n
+Question\nA)\nB)\nC)\nD)\n
 5.Then, you have to provide the answer key and description with proper reasons for every questions. 
 title must be on 'Answer Key'. Also, the answer key should be in list of dictionaries. 
 It will be 2 column: Answer and Explanation as this structure:
 answer_key = [ {'Answer': '', 'Explanation': ""},
-    {'Answer': '', 'Explanation': ""},
     ...........
-    {'Answer': '', 'Explanation': ""},
-    ]
+    {'Answer': '', 'Explanation': ""} ]
 Because I will use this data to st.dataframe.
 """
 
 st.title('Reading exam generator')
-st.markdown('AI ช่วยแต่งข้อสอบพาร์ต Reading เพียงแค่เขียน keyword ที่ต้องการ \n Article จะมีความยาว 300-400 คำ ความยากระดับ B2-C1 และมีข้อสอบทั้งหมด 10 ข้อ')
-user_input = st.text_area('text your keyword here')
+st.markdown('AI ช่วยแต่งข้อสอบพาร์ต Reading เพียงแค่เขียน keyword ที่ต้องการ\nArticle จะมีความยาว 300-400 คำ ความยากระดับ B2-C1 และมีข้อสอบทั้งหมด 10 ข้อ')
+user_input = st.text_input('text your keyword here', 'ex. Wicked')
 
 if st.button('Click'):
     messages_so_far = [
@@ -39,16 +34,13 @@ if st.button('Click'):
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=messages_so_far,
-        temperature=1.1
+        temperature=1.2
     )
     st.markdown('**AI response:**')
     answer = response.choices[0].message.content
     st.write(answer.split("answer_key = ")[0].strip())
 
     answer_string = answer.split("answer_key = ")[1].strip()
-    pattern = r"\{'Answer':\s*'([A-D])',\s*'Explanation':\s*\"(.*?)\"\}"
-    matches = re.findall(pattern, answer_string)
-    answer_key = [{'Answer': answer, 'Explanation': explanation} for answer, explanation in matches]
-    answer_df = pd.DataFrame(answer_key)
-    answer_df.index = ['1', '2', '3','4', '5', '6','7', '8', '9','10']
+    answer_json = json.loads(answer_string)
+    answer_df = pd.DataFrame(answer_json)
     st.dataframe(answer_df)
